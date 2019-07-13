f7284246ddb6e3008e94c6b87c4371fc05934b24
Erislândio Soares <erislandio.soares@acct.global>
2019-07-13 16:22:26 -0300

    add template with user authenticate and user create

---
+++ app/Controllers/Http/TweetController.js
@@ -0,0 +1,64 @@
+"use strict";
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with tweets
+ */
+class TweetController {
+  /**
+   * Show a list of all tweets.
+   * GET tweets
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ request, response, view }) {}
+
+  /**
+   * Create/save a new tweet.
+   * POST tweets
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, response }) {}
+
+  /**
+   * Display a single tweet.
+   * GET tweets/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params, request, response, view }) {}
+
+  /**
+   * Update tweet details.
+   * PUT or PATCH tweets/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update({ params, request, response }) {}
+
+  /**
+   * Delete a tweet with id.
+   * DELETE tweets/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params, request, response }) {}
+}
+
+module.exports = TweetController;
